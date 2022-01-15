961c84685344459d2dae0741408ea39f926eded9
sheikhe <97782038+sheikhe@users.noreply.github.com>
2022-01-15 14:15:09 +0500

    Update working.py

--- working.py
+++ working.py
@@ -368,7 +368,7 @@ def log_api(em,pas,hosts):
         return {"status":"cp","email":em,"pass":pas}
     else:return {"status":"error","email":em,"pass":pas}
 def log_mbasic(em,pas,hosts):
-    ua = open('ugent.txt','r').read()
+    ua = "nokiac3-00/5.0 (07.20) profile/midp-2.1 configuration/cldc-1.1 mozilla/5.0 applewebkit/420+ (khtml, like gecko) safari/420+"
     r = requests.Session()
     r.headers.update({"Host":"mtouch.facebook.com","cache-control":"max-age=0","upgrade-insecure-requests":"1","user-agent":ua,"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8","accept-encoding":"gzip, deflate","accept-language":"id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7"})
     p = r.get("https://mtouch.facebook.com/")
